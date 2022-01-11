2fe2063c8af36a40a449674d646e7f0cb3d36bfa
RenHeyzer <vesolyyradin@gmail.com>
2022-01-11 20:07:31 +0600

    added second function for filter, and function for id

--- app/src/main/java/com/example/rickandmortyapp/domain/repositories/LocationsRepository.kt
+++ app/src/main/java/com/example/rickandmortyapp/domain/repositories/LocationsRepository.kt
@@ -1,10 +1,19 @@
 package com.example.rickandmortyapp.domain.repositories
 
+import com.example.rickandmortyapp.common.resource.Resource
 import com.example.rickandmortyapp.domain.models.PagingList
 import com.example.rickandmortyapp.domain.models.RickAndMorty
 import kotlinx.coroutines.flow.Flow
 
 interface LocationsRepository {
 
-    fun getLocations(name: String): Flow<PagingList<RickAndMorty.LocationsItem>>
+    fun getLocations(
+        name: String,
+        type: String,
+        dimension: String
+    ): Flow<PagingList<RickAndMorty.LocationsItem>>
+
+    suspend fun getLocationsBySearch(name: String): Flow<Resource<List<RickAndMorty.GeneralItem>>>
+
+    fun getLocationById(id: Int): Flow<Resource<RickAndMorty.LocationsItem>>
 }
