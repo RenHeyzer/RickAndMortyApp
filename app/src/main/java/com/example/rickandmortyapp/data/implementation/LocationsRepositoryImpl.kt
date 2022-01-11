c552023fcd839f76512abc527be0295796e155be
RenHeyzer <vesolyyradin@gmail.com>
2022-01-11 20:07:25 +0600

    added second function for filter, and function for id

--- app/src/main/java/com/example/rickandmortyapp/data/implementation/LocationsRepositoryImpl.kt
+++ app/src/main/java/com/example/rickandmortyapp/data/implementation/LocationsRepositoryImpl.kt
@@ -3,6 +3,8 @@ package com.example.rickandmortyapp.data.implementation
 import com.example.rickandmortyapp.base.BaseRepository
 import com.example.rickandmortyapp.data.implementation.pagingsources.LocationsPagingSource
 import com.example.rickandmortyapp.data.network.apiservices.LocationsApiService
+import com.example.rickandmortyapp.data.network.dtos.models.toGeneral
+import com.example.rickandmortyapp.data.network.dtos.models.toLocations
 import com.example.rickandmortyapp.domain.repositories.LocationsRepository
 import javax.inject.Inject
 
@@ -10,5 +12,16 @@ class LocationsRepositoryImpl @Inject constructor(
     private val service: LocationsApiService
 ) : BaseRepository(), LocationsRepository {
 
-    override fun getLocations(name: String) = doPagingRequest(LocationsPagingSource(service, name))
+    override fun getLocations(name: String, type: String, dimension: String) =
+        doPagingRequest(LocationsPagingSource(service, name, type, dimension))
+
+    override suspend fun getLocationsBySearch(name: String) = doRequest {
+        service.getLocations(1, name, "", "").results.map {
+            it.toGeneral()
+        }
+    }
+
+    override fun getLocationById(id: Int) = doRequest {
+        service.getLocationById(id).toLocations()
+    }
 }
