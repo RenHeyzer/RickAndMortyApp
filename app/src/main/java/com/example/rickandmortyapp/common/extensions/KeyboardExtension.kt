47b52be1746d7a545496ee30d90477c052cdddaf
RenHeyzer <vesolyyradin@gmail.com>
2022-01-11 20:08:42 +0600

    deleted unused functions

--- app/src/main/java/com/example/rickandmortyapp/common/extensions/KeyboardExtension.kt
+++ app/src/main/java/com/example/rickandmortyapp/common/extensions/KeyboardExtension.kt
@@ -6,28 +6,11 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 
-fun Activity.hideKeyboard() {
-    hideKeyboard(currentFocus ?: View(this))
-}
-
 fun Fragment.hideKeyboard() {
     view?.let { activity?.hideKeyboard(it) }
-}
-
-fun Activity.showKeyboard() {
-    showKeyboard(currentFocus ?: View(this))
-}
-
-fun Fragment.showKeyboard() {
-    view?.let { activity?.showKeyboard(it) }
 }
 
 fun Context.hideKeyboard(view: View) {
     val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
-}
-
-fun Context.showKeyboard(view: View) {
-    val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    inputMethodManager.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
 }
