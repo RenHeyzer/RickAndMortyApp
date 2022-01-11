09e154eed4361b385710ff66ef845437bdd63255
RenHeyzer <vesolyyradin@gmail.com>
2022-01-11 20:00:53 +0600

    added registerAdapterDataObserver and details view on items

--- app/src/main/java/com/example/rickandmortyapp/presentation/ui/fragments/search/SearchFragment.kt
+++ app/src/main/java/com/example/rickandmortyapp/presentation/ui/fragments/search/SearchFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.widget.SearchView
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.rickandmortyapp.R
 import com.example.rickandmortyapp.base.BaseFragment
@@ -15,10 +17,9 @@ import com.example.rickandmortyapp.common.extensions.setOnActionExpandListener
 import com.example.rickandmortyapp.common.extensions.setTools
 import com.example.rickandmortyapp.common.extensions.submitSearch
 import com.example.rickandmortyapp.databinding.FragmentSearchBinding
+import com.example.rickandmortyapp.presentation.state.UIState
 import com.example.rickandmortyapp.presentation.ui.adapters.SearchAdapter
-import com.example.rickandmortyapp.presentation.ui.adapters.paging.LoadStateAdapter
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>(
@@ -26,7 +27,11 @@ class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>(
 ) {
     override val binding by viewBinding(FragmentSearchBinding::bind)
     override val viewModel by viewModels<SearchViewModel>()
-    private val adapter: SearchAdapter = SearchAdapter()
+    private val adapter: SearchAdapter = SearchAdapter(
+        this::onItemCharacterClick,
+        this::onItemLocationClick,
+        this::onItemEpisodeClick
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,19 +44,53 @@ class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>(
 
     private fun setupRecycler() = with(binding) {
         searchRecycler.layoutManager = LinearLayoutManager(requireContext())
-        searchRecycler.adapter = adapter.withLoadStateFooter(LoadStateAdapter {
-            adapter.retry()
-        })
+        searchRecycler.adapter = adapter
+        registerScroll(adapter)
     }
 
     override fun setupRequests() {
-        viewModel.processAllRequest("")
+        viewModel.processAllRequests("")
     }
 
     override fun setupObserves() {
-        viewModel.allState.observe(viewLifecycleOwner, {
-            lifecycleScope.launch {
-                adapter.submitData(it)
+        binding.apply {
+            viewModel.generalState.observe(viewLifecycleOwner, {
+                searchLoading.isVisible = it is UIState.Loading
+                when (it) {
+                    is UIState.Loading -> {
+                    }
+                    is UIState.Error -> {
+                    }
+                    is UIState.Success -> {
+                        val sortedList =
+                            it.data.sortedByDescending { date -> date.created }
+                        adapter.submitList(sortedList)
+                    }
+                }
+            })
+        }
+    }
+
+    private fun registerScroll(adapter: SearchAdapter) = with(binding.searchRecycler) {
+        adapter.registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
+            override fun onChanged() {
+                scrollToPosition(0)
+            }
+
+            override fun onItemRangeRemoved(positionStart: Int, itemCount: Int) {
+                scrollToPosition(0)
+            }
+
+            override fun onItemRangeMoved(fromPosition: Int, toPosition: Int, itemCount: Int) {
+                scrollToPosition(0)
+            }
+
+            override fun onItemRangeChanged(positionStart: Int, itemCount: Int) {
+                scrollToPosition(0)
+            }
+
+            override fun onItemRangeChanged(positionStart: Int, itemCount: Int, payload: Any?) {
+                scrollToPosition(0)
             }
         })
     }
@@ -60,12 +99,33 @@ class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>(
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.toolbar_menu, menu)
         val searchItem = menu.findItem(R.id.search)
+        val filterItem = menu.findItem(R.id.filter)
         val searchView = searchItem.actionView as SearchView
+
+        filterItem.isVisible = false
 
         searchView.setTools(context)
 
-        searchView.submitSearch { viewModel.processAllRequest(it.toString()) }
+        searchView.submitSearch { viewModel.processAllRequests(it.toString()) }
 
         searchItem.setOnActionExpandListener(searchView) { hideKeyboard() }
+    }
+
+    private fun onItemCharacterClick(id: Int, name: String) {
+        val action =
+            SearchFragmentDirections.actionSearchFragmentToCharacterDetailFragment(id, name)
+        findNavController().navigate(action)
+    }
+
+    private fun onItemLocationClick(id: Int, name: String) {
+        val action =
+            SearchFragmentDirections.actionSearchFragmentToLocationDetailFragment(id, name)
+        findNavController().navigate(action)
+    }
+
+    private fun onItemEpisodeClick(id: Int, name: String) {
+        val action =
+            SearchFragmentDirections.actionSearchFragmentToEpisodeDetailFragment(id, name)
+        findNavController().navigate(action)
     }
 }
